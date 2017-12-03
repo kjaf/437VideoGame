b12cd1e5ab4e597a3007df80858a4b4702556192
kjaf <kfetterm@iupui.edu>
2017-12-03 15:15:05 -0500

    added rotation

--- gravityGame/main.lua
+++ gravityGame/main.lua
@@ -3,21 +3,44 @@ function love.load()
     Astronaut = {}
     Astronaut.image =  love.graphics.newImage("astronaut.png")
     Astronaut.x = 100
-    Astronaut.y = 100
-    text = "nothing"
+    Astronaut.y = 100 
+    angle = 0
+    speed = 10
+    astronautR = 0
+
 
 end
  
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
-	if love.keyboard.isDown("space") then
-      Astronaut.image = love.graphics.newImage("boosting.png")
-   end
+	if love.keyboard.isDown("right") then
+      astronautR = astronautR + .004
+      text = "rotating"
+  end
+
+  	if love.keyboard.isDown("left") then
+      astronautR = astronautR + -.004
+      text = "rotating"
+  end
+
+
+  -- if love.keyboard.isDown("right") + love.keyboard.isDown("space") then 
+  -- 	Astronaut.image = love.graphics.newImage("boosting.png")
+  -- end
+ -- Astronaut.x = Astronaut.x + (math.cos(math.rad(angle)) * speed * dt)
+ -- Astronaut.y = Astronaut.y + (math.cos(math.rad(angle)) * speed * dt)
+ textx = Astronaut.x 
+ texty = Astronaut.y
+
+	
+
+
    
 end
  
 -- Draw a coloured rectangle.
 function love.draw()
-    love.graphics.draw(Astronaut.image, Astronaut.x, Astronaut.y)
-    love.graphics.print(text, 300,300)
+    love.graphics.draw(Astronaut.image, Astronaut.x, Astronaut.y,astronautR)
+    love.graphics.print(textx, 300,300)
+    love.graphics.print(texty, 400,400)
 end
