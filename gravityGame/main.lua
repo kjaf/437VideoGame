3a4f3915af22e6938beaa3c3ec641c192dc3f9c0
kjaf <kfetterm@iupui.edu>
2017-12-03 16:26:42 -0500

    added the first planet and scaled the astronaut

--- gravityGame/main.lua
+++ gravityGame/main.lua
@@ -1,24 +1,51 @@
 
 function love.load()
     Astronaut = {}
+    Planets = {}
 
-    Astronaut.image =  love.graphics.newImage("/images/astronaut.png")
+
+    for i = 1, 3 do
+      Planets[i] = {}
+    end
+
+
+
+
+      Planets[1].image = love.graphics.newImage("/images/planets/planet1.png")
+      Planets[1].x = 800
+      Planets[1].y = 800
+      Planets[1].mass = 1000 
+
+
+
+
+
+    Astronaut.image =  love.graphics.newImage("/images/player/astronaut.png")
     Astronaut.x = 100
-    Astronaut.y = 100 
+    Astronaut.y = 100
+    Astronaut.mass = 1  
+    
+
+
     angle = 0
     speed = 2
     astronautR = 0
     text = "nothing"
+
+
+    planet = {}
+
   
 
 
     function love.keyreleased(key)
       if key == "space" then
-        Astronaut.image = love.graphics.newImage("/images/astronaut.png")
+        Astronaut.image = love.graphics.newImage("/images/player/astronaut.png")
       end
     end
 
 
+    love.window.setMode(1500,1000)
 
 
 
@@ -28,16 +55,16 @@ end
 
 function love.update(dt)
 	if love.keyboard.isDown("right") then
-      astronautR = astronautR + .01
+      astronautR = astronautR + .03
   end
 
   	if love.keyboard.isDown("left") then
-      astronautR = astronautR + -.01
+      astronautR = astronautR + -.03
   end
 
 
   if (love.keyboard.isDown("space")) then 
-  	Astronaut.image = love.graphics.newImage("/images/boosting.png")
+  	Astronaut.image = love.graphics.newImage("/images/player/boosting.png")
     Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
     Astronaut.y = Astronaut.y + math.sin(astronautR) * speed 
   end
@@ -57,7 +84,8 @@ end
  
 
 function love.draw()
-    love.graphics.draw(Astronaut.image, Astronaut.x, Astronaut.y,astronautR)
+    love.graphics.draw(Astronaut.image, Astronaut.x, Astronaut.y,astronautR,.75,.75)
+    love.graphics.draw(Planets[1].image, Planets[1].x, Planets[1].y,0,3,3)
     
     
 end
