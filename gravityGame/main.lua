f7bedb6c179f83496a67a1425c45c312587ed744
kjaf <kfetterm@iupui.edu>
2017-12-03 14:25:05 -0500

    astronaut on screen

--- gravityGame/main.lua
+++ gravityGame/main.lua
@@ -1,16 +1,15 @@
 -- Load some default values for our rectangle.
 function love.load()
-    x, y, w, h = 20, 20, 60, 20
+    Astronaut = {}
+    Astronaut.image =  love.graphics.newImage("astronaut.png")
 end
  
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
-    w = w + 1
-    h = h + 1
+   
 end
  
 -- Draw a coloured rectangle.
 function love.draw()
-    love.graphics.setColor(0, 100, 100)
-    love.graphics.rectangle("fill", x, y, w, h)
+    love.graphics.draw(Astronaut.image, 100, 100)
 end
