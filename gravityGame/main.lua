3223a0d626dae619b668668f2e3c582d2c89c40d
kjaf <kfetterm@iupui.edu>
2017-12-03 14:33:39 -0500

    first keyboard input

--- gravityGame/main.lua
+++ gravityGame/main.lua
@@ -2,14 +2,22 @@
 function love.load()
     Astronaut = {}
     Astronaut.image =  love.graphics.newImage("astronaut.png")
+    Astronaut.x = 100
+    Astronaut.y = 100
+    text = "nothing"
+
 end
  
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
+	if love.keyboard.isDown("space") then
+      Astronaut.image = love.graphics.newImage("boosting.png")
+   end
    
 end
  
 -- Draw a coloured rectangle.
 function love.draw()
-    love.graphics.draw(Astronaut.image, 100, 100)
+    love.graphics.draw(Astronaut.image, Astronaut.x, Astronaut.y)
+    love.graphics.print(text, 300,300)
 end
