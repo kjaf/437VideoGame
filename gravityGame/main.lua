f16262fce17c6fd496b3f30e8f2f4ad5dcdd17c7
taylor-peavey <taylorpeavey@gmail.com>
2017-12-04 20:33:20 -0500

    add in score, add in fuel guage
    scoring mechanism: start at 60 and go down 1 every update (every second)
    fuel guage: for practice purposes start at 20 and go down but this can be changed

--- gravityGame/main.lua
+++ gravityGame/main.lua
@@ -1,15 +1,23 @@
 
 function love.load()
     Astronaut = {}
-	Asteroids = {}
+	  Asteroids = {}
     Planets = {}
+    Fuel = {}
+    score = 60
     force = 0 
 
-	angle = 0
+	  angle = 0
     speed = 2
     astronautR = 0
     text = "nothing"
     text1 = "nothing"
+
+    --can change starting fuel amount here
+    Fuel.image =  love.graphics.newImage("/images/HUD/fuel100.png")
+    Fuel.x = 50
+    Fuel.y = 500
+    Fuel.amount = 20
 
 
     function love.keyreleased(key)
@@ -22,7 +30,7 @@ function love.load()
     love.window.setMode(800,600)
 	
 		function createAstronaut()
-	    Astronaut = {}
+	  Astronaut = {}
 		Astronaut.image =  love.graphics.newImage("/images/player/astronaut.png")
 		Astronaut.x = 100
 		Astronaut.y = 100
@@ -120,6 +128,14 @@ end
  
 
 function love.update(dt)
+  --score goes down every update (every second)
+  score = score - dt
+  intScore = math.ceil(score)
+  --if score is <= 0, stay at 0
+  if intScore <= 0 then
+     intScore = 0
+  end
+
 	if love.keyboard.isDown("right") then
       astronautR = astronautR + .03
   end
@@ -128,11 +144,53 @@ function love.update(dt)
       astronautR = astronautR + -.03
   end
 
-
   if (love.keyboard.isDown("space")) then 
-  	Astronaut.image = love.graphics.newImage("/images/player/boosting.png")
-    Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
-    Astronaut.y = Astronaut.y + math.sin(astronautR) * speed 
+    Fuel.amount = Fuel.amount - dt
+    Astronaut.image = love.graphics.newImage("/images/player/boosting.png")  
+    if Fuel.amount <= 100 and Fuel.amount > 90 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel100.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed     
+    elseif Fuel.amount <= 90 and Fuel.amount > 80 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel90.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed    
+    elseif Fuel.amount <= 80 and Fuel.amount > 70 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel80.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed    
+    elseif Fuel.amount <= 70 and Fuel.amount > 60 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel70.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed    
+    elseif Fuel.amount <= 60 and Fuel.amount > 50 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel60.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed       
+    elseif Fuel.amount <= 50 and Fuel.amount > 40 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel50.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed       
+    elseif Fuel.amount <= 40 and Fuel.amount > 30 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel40.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed       
+    elseif Fuel.amount <= 30 and Fuel.amount > 20 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel30.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed     
+    elseif Fuel.amount <= 20 and Fuel.amount > 10 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel20.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed    
+    elseif Fuel.amount <= 10 and Fuel.amount > 0 then
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel10.png")
+      Astronaut.x = Astronaut.x + math.cos(astronautR) * speed
+      Astronaut.y = Astronaut.y + math.sin(astronautR) * speed      
+    elseif Fuel.amount <= 0 then   
+      Fuel.image = love.graphics.newImage("/images/HUD/fuel0.png")
+      Astronaut.image =  love.graphics.newImage("/images/player/astronaut.png")
+    end 
   end
 
   force = checkGravity(Planets[1].mass)
@@ -144,7 +202,8 @@ end
  
 
 function love.draw()
-    love.graphics.draw(Astronaut.image, Astronaut.x, Astronaut.y,astronautR,.6,.6)
+  love.graphics.draw(Fuel.image, Fuel.x, Fuel.y)
+  love.graphics.draw(Astronaut.image, Astronaut.x, Astronaut.y,astronautR,.6,.6)
 	
 	for i in pairs(Asteroids) do
 		love.graphics.draw(Asteroids[i].image, Asteroids[i].x, Asteroids[i].y,0,.75,.75)
@@ -155,6 +214,8 @@ function love.draw()
 	end
 	
     love.graphics.print(text, 300,300)
+    love.graphics.print('Score: ', 670, 10, 0, 2, 2)
+    love.graphics.print(intScore, 760, 10, 0, 2, 2)  
     
     
 end
